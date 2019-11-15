c2bdfc31af1fd6aea987351a9b1046770c7fc05a
Ant <ryu4979@gmail.com>
2019-11-16 04:16:42 +0900

    style: Twitter.hsのインデント整形

--- src/Twitter.hs
+++ src/Twitter.hs
@@ -18,29 +18,29 @@ instance ToJSON Tweet
 
 tweet :: Text -> IO ()
 tweet tw = do
-  req     <- parseRequest "https://api.twitter.com/1.1/statuses/update.json"
-  manager <- newManager tlsManagerSettings
-  let postReq = urlEncodedBody [("status", encodeUtf8 tw)] req
-  signedReq <- signOAuth twOAuth twCredential postReq
+  req         <- parseRequest "https://api.twitter.com/1.1/statuses/update.json"
+  manager     <- newManager tlsManagerSettings
+  let postReq  = urlEncodedBody [("status", encodeUtf8 tw)] req
+  signedReq   <- signOAuth twOAuth twCredential postReq
   httpLbs signedReq manager
   return ()
 
 fav :: Text -> IO ()
 fav twId = do
-  req     <- parseRequest "https://api.twitter.com/1.1/favorites/create.json"
-  manager <- newManager tlsManagerSettings
-  let postReq = urlEncodedBody [("id", encodeUtf8 twId)] req
-  signedReq <- signOAuth twOAuth twCredential postReq
+  req         <- parseRequest "https://api.twitter.com/1.1/favorites/create.json"
+  manager     <- newManager tlsManagerSettings
+  let postReq  = urlEncodedBody [("id", encodeUtf8 twId)] req
+  signedReq   <- signOAuth twOAuth twCredential postReq
   httpLbs signedReq manager
   return ()
 
 userTimeline :: String -> Int -> IO (Either String [Tweet])
 userTimeline screenName count = do
-  req <- parseRequest
-    $ "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    ++ "?screen_name=" ++ screenName
-    ++ "&count=" ++ (show count)
+  req       <- parseRequest
+              $ "https://api.twitter.com/1.1/statuses/user_timeline.json"
+              ++ "?screen_name=" ++ screenName
+              ++ "&count=" ++ (show count)
   signedReq <- signOAuth twOAuth twCredential req
   manager   <- newManager tlsManagerSettings
-  res <- httpLbs signedReq manager
+  res       <- httpLbs signedReq manager
   return $ eitherDecode $ responseBody res
