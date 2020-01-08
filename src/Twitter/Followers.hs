0991e20ba39f92f73530bd3ecb0089a4d2562ebf
Ant <ryu4979@gmail.com>
2020-01-09 00:37:46 +0900

    feat: followerListの追加

--- src/Twitter/Followers.hs
+++ src/Twitter/Followers.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 
-module Twitter.Followers (FollowerIdsResult (..), followerIds) where
+module Twitter.Followers (  FollowerIdsResult (..)
+                          , FollowerListsResult (..)
+                          , FollowerListRequest
+                          , makeFollowerListRequest
+                          , followerIds
+                          , followerList
+                          ) where
 
 import Data.Aeson
 import GHC.Generics
 import Network.HTTP.Conduit
 import Twitter.TwSettings
+import Twitter.Data.User
 
 data FollowerIdsResult = FollowerIdsResult {
   ids                 :: [Integer],
@@ -18,10 +26,39 @@ data FollowerIdsResult = FollowerIdsResult {
 instance FromJSON FollowerIdsResult
 instance ToJSON   FollowerIdsResult
 
+data FollowerListsResult = FollowerListsResult {
+  users               :: [User],
+  next_cursor         :: Integer,
+  next_cursor_str     :: String,
+  previous_cursor     :: Integer,
+  previous_cursor_str :: String
+} deriving (Show, Generic)
+instance FromJSON FollowerListsResult
+instance ToJSON   FollowerListsResult
+
+data FollowerListRequest = FollowerListRequest {
+  screenName :: String,
+  count      :: Int
+} deriving (Show, Generic)
+
+makeFollowerListRequest :: String -> Int -> FollowerListRequest
+makeFollowerListRequest screenName count
+  | count < 1 || 200 < count = error "count range: 1 <= count <= 200"
+  | otherwise                = FollowerListRequest { screenName = screenName, count = count }
+
 followerIds :: String -> IO (Either String FollowerIdsResult)
 followerIds screenName = do
   req       <- parseRequest
               $ "https://api.twitter.com/1.1/followers/ids.json"
               ++ "?screen_name=" ++ screenName
+  res       <- requestTwitterApi req
+  return $ eitherDecode $ responseBody res
+
+followerList :: FollowerListRequest -> IO (Either String FollowerListsResult)
+followerList fRequest = do
+  req       <- parseRequest
+              $ "https://api.twitter.com/1.1/followers/list.json"
+              ++ "?screen_name=" ++ screenName fRequest
+              ++ "&count=" ++ show (count fRequest)
   res       <- requestTwitterApi req
   return $ eitherDecode $ responseBody res
--- test/Twitter/FollowersSpec.hs
+++ test/Twitter/FollowersSpec.hs
@@ -3,10 +3,17 @@
 module Twitter.FollowersSpec (spec) where
 
 import Test.Hspec
+import Control.Exception (evaluate)
 import Twitter.Followers
 
 spec :: Spec
 spec = do
+  describe "makeFollowerListRequestのテスト" $ do
+    it "countが不正(1未満)" $ do
+      evaluate (makeFollowerListRequest "ant2357" 0) `shouldThrow` errorCall "count range: 1 <= count <= 200"
+    it "countが不正(200超え)" $ do
+      evaluate (makeFollowerListRequest "ant2357" 201) `shouldThrow` errorCall "count range: 1 <= count <= 200"
+
   describe "フォロワー一覧の取得テスト" $ do
     let screenName    = "ant2357"
     let badScreenName = "ant2357_run"
@@ -19,6 +26,18 @@ spec = do
 
     it "実在しないユーザーのフォロワー一覧の取得(IDリスト)" $ do
       res <- followerIds badScreenName
+      case res of
+        Left  _ -> "goodcase" `shouldBe` "goodcase"
+        Right _ -> "bad"      `shouldBe` "case"
+
+    it "フォロワー一覧の取得(ユーザーオブジェクト)" $ do
+      res <- followerList $ makeFollowerListRequest screenName 200
+      case res of
+        Left  _      -> "bad"                     `shouldBe` "case"
+        Right fUsers -> ((length . users) fUsers) `shouldBe` 200
+
+    it "実在しないユーザーのフォロワー一覧の取得(ユーザーオブジェクト)" $ do
+      res <- followerList $ makeFollowerListRequest badScreenName 200
       case res of
         Left  _ -> "goodcase" `shouldBe` "goodcase"
         Right _ -> "bad"      `shouldBe` "case"
