6d4b62bacb530ed398d7d70316ced37f9e74176a
Ant <ryu4979@gmail.com>
2020-01-12 04:26:00 +0900

    feat: lookupの追加

--- src/Twitter/Friendships.hs
+++ src/Twitter/Friendships.hs
@@ -1,12 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
 
-module Twitter.Friendships (follow, unFollow) where
+module Twitter.Friendships (Relationship (..)
+                            , follow
+                            , unFollow
+                            , lookup
+                            ) where
 
 import qualified Data.ByteString.Char8 as B8
+import Data.Text
 import Data.Aeson
+import GHC.Generics
 import Network.HTTP.Conduit
 import Twitter.TwSettings
 import Twitter.Data.User
+import Prelude hiding (lookup)
+
+data Relationship = Relationship {
+  name        :: Text,
+  screen_name :: String,
+  id          :: Integer,
+  connections :: [String]
+} deriving (Show, Generic)
+instance FromJSON Relationship
+instance ToJSON   Relationship
 
 follow :: String -> IO (Either String User)
 follow screenName = do
@@ -21,3 +41,22 @@ unFollow screenName = do
   let postReq  = urlEncodedBody [("screen_name", B8.pack screenName)] req
   res         <- requestTwitterApi postReq
   return $ eitherDecode $ responseBody res
+
+class Lookup a where
+  lookup :: a -> IO (Either String [Relationship])
+
+instance (Num a, Show a, Eq a) => Lookup a where
+  lookup userId = do
+    req <- parseRequest
+            $ "https://api.twitter.com/1.1/friendships/lookup.json"
+            ++ "?user_id=" ++ show userId 
+    res <- requestTwitterApi req
+    return $ eitherDecode $ responseBody res
+
+instance Lookup String where
+  lookup screenName = do
+    req <- parseRequest
+            $ "https://api.twitter.com/1.1/friendships/lookup.json"
+            ++ "?screen_name=" ++ screenName
+    res <- requestTwitterApi req
+    return $ eitherDecode $ responseBody res
--- test/Twitter/FriendshipsSpec.hs
+++ test/Twitter/FriendshipsSpec.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 
 module Twitter.FriendshipsSpec (spec) where
 
 import Test.Hspec
 import Twitter.Friendships
-import Twitter.Data.User
+import Twitter.Data.User 
+import Prelude hiding (lookup)
 
 spec :: Spec
 spec = do
@@ -14,7 +16,7 @@ spec = do
       res <- follow screenName
       case res of
         Left  _  -> "bad"            `shouldBe` "case"
-        Right tw -> (screen_name tw) `shouldBe` screenName
+        Right tw -> (screen_name (tw :: User)) `shouldBe` screenName
 
     it "二重フォロー" $ do
       res <- follow screenName
@@ -27,10 +29,24 @@ spec = do
       res <- unFollow screenName
       case res of
         Left  _  -> "bad"            `shouldBe` "case"
-        Right tw -> (screen_name tw) `shouldBe` screenName
+        Right tw -> (screen_name (tw :: User)) `shouldBe` screenName
 
     it "二重アンフォロー" $ do
       res <- unFollow screenName
       case res of
         Left  _  -> "bad"          `shouldBe` "case"
         Right tw -> (following tw) `shouldBe` False
+
+  describe "lookupのテスト" $ do
+    let screenName = "strimuer213p" :: String
+    it "自分と対象ユーザーの関係を調べる(数値)" $ do
+      res <- lookup 4454609233
+      case res of
+        Left  _  -> "bad"                                     `shouldBe` "case"
+        Right xs -> (screen_name ((head xs) :: Relationship)) `shouldBe` screenName
+
+    it "自分と対象ユーザーの関係を調べる(String)" $ do
+      res <- lookup screenName
+      case res of
+        Left  _  -> "bad"                                     `shouldBe` "case"
+        Right xs -> (screen_name ((head xs) :: Relationship)) `shouldBe` screenName
