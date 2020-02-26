e77a7925e5b7a8c3f4c1114d42d1c67e828c1616
Ant <ryu4979@gmail.com>
2020-02-27 02:05:55 +0900

    refactor: AutoFollow.hsのリファクタリング

--- app/AutoFollow.hs
+++ app/AutoFollow.hs
@@ -11,6 +11,18 @@ import Twitter.Friendships (follow)
 import Twitter.Data.Tweet
 import Twitter.Data.User
 
+followTimeline :: [Tweet] -> IO ()
+followTimeline tweets = do
+  let users       = Set.toList . Set.fromList $ map user tweets
+  let followUsers = filter (not . following) users
+  let followNames = map (screen_name) followUsers
+  forM_ followNames $ \followName -> do
+    res <- follow followName
+    case res of
+      Left  err -> putStrLn $ "BAD:" ++ err
+      Right u   -> putStrLn $ "SUCCESS:" ++ (screen_name u)
+    threadDelay (5 * 60 * 1000 * 1000)
+
 main :: IO ()
 main = do
     let searchQ     = "#swallows"
@@ -19,13 +31,4 @@ main = do
     res <- search $ makeSearchRequest searchQ "mixed" searchCount
     case res of
       Left  err -> error err
-      Right tl  -> do
-        let users       = Set.toList . Set.fromList $ map user (statuses tl)
-        let followUsers = filter (not . following) users
-        let followNames = map (screen_name) followUsers
-        forM_ followNames $ \followName -> do
-          threadDelay (5 * 60 * 1000 * 1000)
-          res <- follow followName
-          case res of
-            Left  err -> putStrLn $ "BAD:" ++ err
-            Right u   -> putStrLn $ "SUCCESS:" ++ (screen_name u)
+      Right tl  -> followTimeline (statuses tl)
