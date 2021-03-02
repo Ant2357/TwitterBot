ae6cb4076efc89233a260107a66e7c586d1fd6e9
Ant <ryu4979@gmail.com>
2021-03-03 00:39:00 +0900

    fix: 名前の変更(MediaInfo -> Media)

--- src/Twitter/Media.hs
+++ src/Twitter/Media.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-module Twitter.Media (MediaInfo (..), mediaUpload) where
+module Twitter.Media (Media (..), mediaUpload) where
 
 import qualified Data.ByteString.Char8  as B8
 import qualified Data.ByteString.Base64 as B64
@@ -10,15 +10,15 @@ import GHC.Generics
 import Network.HTTP.Conduit
 import Twitter.TwSettings
 
-data MediaInfo = MediaInfo {
+data Media = Media {
   media_id :: Integer,
   size     :: Integer
 } deriving (Show, Generic)
 
-instance FromJSON MediaInfo
-instance ToJSON   MediaInfo
+instance FromJSON Media
+instance ToJSON   Media
 
-mediaUpload :: B8.ByteString -> IO (Either String MediaInfo)
+mediaUpload :: B8.ByteString -> IO (Either String Media)
 mediaUpload mediaData = do
   req         <- parseRequest "https://upload.twitter.com/1.1/media/upload.json"
   let postReq  = urlEncodedBody [("media_data", B64.encode mediaData)] req
